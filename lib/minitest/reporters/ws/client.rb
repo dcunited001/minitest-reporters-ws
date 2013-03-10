0947bb51e2ccd206ba095397537399e67b4dfc1f
David Conner <dconner.pro@gmail.com>
2013-03-09 19:48:53 -0500

    Minitest::Reporters::Ws::Client: created a client class

---
+++ lib/minitest/reporters/ws/client.rb
@@ -0,0 +1,16 @@
+module Minitest
+  module Reporters
+    module Ws
+      class Client
+        def initialize(opts = {})
+
+        end
+
+        def notify(*msgs)
+
+        end
+
+      end
+    end
+  end
+end
