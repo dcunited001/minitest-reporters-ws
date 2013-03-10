a77c818f7b08d8d2c8975bd34981bb4f4933ae83
David Conner <dconner.pro@gmail.com>
2013-03-09 21:00:32 -0500

    Changed module definitions

--- lib/minitest/reporters/ws.rb
+++ lib/minitest/reporters/ws.rb
@@ -2,5 +2,14 @@
 require 'ansi/code'
 require 'minitest/reporters'
 
+require 'web_socket'
+
+module Minitest
+  module Reporters
+    module Ws
+    end
+  end
+end
+
 require 'minitest/reporters/ws/reporter'
 require 'minitest/reporters/ws/client'
