19501fbf17ab8336eb097db4faeee7b19d7a0ab8
David Conner <dconner.pro@gmail.com>
2013-03-10 15:12:57 -0400

    fixed error where zeus randomly crashes after a few test runs.... and takes iTerm down with it LOL FML... not so easy to troubleshoot

--- lib/minitest/reporters/ws/client.rb
+++ lib/minitest/reporters/ws/client.rb
@@ -16,14 +16,14 @@ module Minitest::Reporters::Ws
     def identify
       data = { receiver: "server", method: "identify", arguments: ["rspec"] }
       @socket.send(data.to_json) if connected?
-    rescue => ex
-      puts ex.message
+    #rescue => ex
+    #  puts ex.message
     end
 
     def send_msg(data)
       @socket.send(data.to_json) if connected?
-    rescue => ex
-      puts ex.message
+    #rescue => ex
+    #  puts ex.message
     end
 
     def close
@@ -31,8 +31,8 @@ module Minitest::Reporters::Ws
       data = { receiver: "server", method: "disconnect", arguments: ["rspec"] }
       @socket.send(data.to_json) if connected?
       @socket.close if connected?
-    rescue => ex
-      puts ex.message
+    #rescue => ex
+    #  puts ex.message
     end
 
     def connected?
@@ -45,9 +45,9 @@ module Minitest::Reporters::Ws
       @timestamp = Time.now.to_i
       @socket = begin
         WebSocket.new("ws://localhost:10081")
-      rescue => ex
-        puts ex.message
-        nil
+      #rescue => ex
+      #  puts ex.message
+      #  nil
       end
     end
 
