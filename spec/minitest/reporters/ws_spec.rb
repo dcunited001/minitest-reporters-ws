4b957cb2eb37c064161c02964f044c882b0e080f
David Conner <dconner.pro@gmail.com>
2013-03-10 00:01:31 -0500

    added error and skipping tests

--- spec/minitest/reporters/ws_spec.rb
+++ spec/minitest/reporters/ws_spec.rb
@@ -13,6 +13,9 @@ describe "Passing Tests" do
 end
 
 describe "Failing Tests" do
+  it "skips 1" do
+    skip "testing"
+  end
   it "fails 1" do
     1.must_equal 3
   end
@@ -21,5 +24,17 @@ describe "Failing Tests" do
   end
   it "fails 3" do
     3.must_equal 1
+  end
+end
+
+describe "Err the Tests" do
+  it "errors 1" do
+    ['Intermitt Ant, the Evil', nil].sample.each_char { |c| c.to_s }
+  end
+  it "errors 2" do
+    UndefDaConstant
+  end
+  it "errors 3" do
+    nil.blah
   end
 end
