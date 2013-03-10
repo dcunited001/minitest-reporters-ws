86c6ea5899186b9801428ee6d8a44f4a58af0165
David Conner <dconner.pro@gmail.com>
2013-03-10 00:02:26 -0500

    moved tests to using only the reporter for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require "minitest/spec"
 require "mocha/setup"
-#require "pry"
+require "pry"
 
 require "minitest/reporters"
 require 'minitest/reporters/ws'
@@ -15,3 +15,11 @@ YML = File.join(File.dirname(__FILE__), 'config', 'ws_reporter.yml')
 def root_path
   File.dirname(__FILE__)
 end
+
+emoji = {
+    'P' => "\u{1F49A} ", # heart
+    'E' => "\u{1f525} ", # flame
+    'F' => "\u{1f4a9} ", # poop
+    'S' => "\u{1f37a} "} # beer
+
+Minitest::Reporters.use! Minitest::Reporters::Ws::Reporter.new(emoji: emoji)
--- spec/spec_helper_integration.rb
+++ spec/spec_helper_integration.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 # in test/support/minitest_reporters.rb
 #require "minitest/reporters"
 #require 'minitest/reporters/ws'
+#
+#emoji = {
+#    'P' => "\u{1F49A} ", # heart
+#    'E' => "\u{1f525} ", # flame
+#    'F' => "\u{1f4a9} ", # poop
+#    'S' => "\u{1f37a} "} # beer
 
-emoji = {
-    'P' => "\u{1F49A} ", # heart
-    'E' => "\u{1f525} ", # flame
-    'F' => "\u{1f4a9} ", # poop
-    'S' => "\u{1f37a} "} # beer
-
-Minitest::Reporters.use! Minitest::Reporters::Ws::Reporter.new(emoji: emoji)
+#Minitest::Reporters.use! Minitest::Reporters::Ws::Reporter.new(emoji: emoji)
